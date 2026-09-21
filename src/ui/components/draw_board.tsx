f641a82eb7d5562ded5d208867a8196daf8acb8e
boterop <boterop22@gmail.com>
2026-09-21 11:13:42 -0500

    Draw buttons

---
+++ src/ui/components/draw_board.tsx
@@ -0,0 +1,9 @@
+export const DrawBoard = ({
+  board,
+  onPress,
+}: {
+  board: number[][];
+  onPress: (number: number) => void;
+}) => {
+  return <></>;
+};
--- src/ui/scenes/game.tsx
+++ src/ui/scenes/game.tsx
@@ -1,14 +1,50 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../AppNavigator';
 import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { Game } from '../../domain/entity';
+import { createGame } from '../../domain/service';
+import { DrawBoard, NumbersButtons } from '../components';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Game'>;
 
 export const GameScreen = ({ route }: Props) => {
   const { difficulty } = route.params;
+
+  const [game, setGame] = useState<Game | null>(null);
+  const [selectedCell, setSelectedCell] = useState<number[]>();
+
+  useEffect(() => {
+    setGame(createGame(difficulty));
+  }, [difficulty]);
+
+  const selectCell = useCallback((number: number) => {
+    setSelectedCell([number]);
+  }, []);
+
+  const pressNumber = useCallback(
+    (number: number) => {
+      const row = selectedCell![0];
+      const col = selectedCell![1];
+
+      if (!game || !row || !col) {
+        return;
+      }
+
+      game.board[row][col] = number;
+    },
+    [selectedCell, game],
+  );
+
   return (
     <View style={styles.container}>
-      <Text>{difficulty}</Text>
+      {!game && <Text>Creating {difficulty} game...</Text>}
+      {game && (
+        <>
+          <DrawBoard board={game.board} onPress={selectCell} />
+          <NumbersButtons onPress={pressNumber} />
+        </>
+      )}
     </View>
   );
 };
